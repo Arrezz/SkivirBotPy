37fddccd6f305bfbe355ae6e5c66dc399e92201e
Arrezz <hansarvid96@hotmail.se>
2020-11-22 14:57:34 +0100

    Security fixes

--- src/main.py
+++ src/main.py
@@ -3,10 +3,12 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='>')
 
+with open('secret.txt') as f:
+    token = f.readline()
 
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
 
-bot.run('token')
+bot.run(token)
