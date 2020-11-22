8192ae533c680eb9aa26953c06f8f67f0ada9b78
Arrezz <hansarvid96@hotmail.se>
2020-11-22 15:18:51 +0100

    Added bot example

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 bot = commands.Bot(command_prefix='>')
 
-with open('secret.txt') as f:
+with open('../secret.txt') as f:
     token = f.readline()
 
 @bot.command()
